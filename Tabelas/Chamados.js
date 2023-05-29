138b93fe9211e9905a7a60908f17bcebff72057f
luizlopes <luizalcorreia28@gmail.com>
2023-05-29 18:41:04 -0300

    Alteração na tabela

--- Tabelas/Chamados.js
+++ Tabelas/Chamados.js
@@ -2,47 +2,46 @@ const Sequelize = require('sequelize');
 const db = require('./db');
 const User = require('./User');
 
-const chamados = db.define('chamados', {
-    idChamados: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    local: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    apartamento: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    bloco: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    ocorrencia: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    anexo: {
-        type: Sequelize.BLOB,
-    },
-    descricao: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    usuarioId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-            model: User,
-            key: 'id'
+const Chamados = db.define('chamados', {
+  idChamados: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  local: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  apartamento: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  bloco: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  ocorrencia: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  anexo: {
+    type: Sequelize.BLOB,
+  },
+  descricao: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  usuarioId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'idUsuario'
     }
-}
+  }
 });
 
-//Criar a tabela
-//chamados.sync();
+//Chamados.sync();
 
-module.exports = chamados;
+module.exports = Chamados;
