0a79f4c83563c3cc83a246162ebc6a0a3ff11552
luizlopeslpc28 <luizalcorreia28@gmail.com>
2023-06-05 15:15:53 -0300

    Iniciando Projeto back

--- conexao/db.js
+++ conexao/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("condoeasy", "root", "130328Sl.", {
+const sequelize = new Sequelize("condoeasy", "root", "root", {
     host: "localhost",
     dialect: "mysql"
 });
