9b558793f6402828a9c351f56068c4fdad9d7bc1
luizlopeslpc28 <125518860+luizlopeslpc28@users.noreply.github.com>
2023-06-07 11:03:05 -0300

    Documentação

--- conexao/db.js
+++ conexao/db.js
@@ -1,10 +1,13 @@
+//Importação do Módulo
 const Sequelize = require('sequelize');
 
+//Definindo estrutura de conexão com o BD (Mysql)
 const sequelize = new Sequelize("condoeasy", "root", "root", {
     host: "localhost",
     dialect: "mysql"
 });
 
+//Tratamento de conexão com o Banco
 sequelize.authenticate()
 .then(() => {
     console.log("Conexão com o banco de dados realizado com sucesso!");
@@ -12,4 +15,5 @@ sequelize.authenticate()
     console.log("Erro: Conexão com o banco de dados não realizado com sucesso! Erro gerado: " + erro);
 });
 
+//Exportação do modelo de dados:
 module.exports = sequelize;
