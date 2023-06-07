771593ce9bfad60d3a15e880f04d7a4f838d526f
luizlopeslpc28 <125518860+luizlopeslpc28@users.noreply.github.com>
2023-06-07 10:17:08 -0300

    documentação do app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+//Importação de módulos:
 const express = require('express');
 const app = express();
 
+//Importação das rotas:
 const usersRoutes = require('./routes/users');
 const chamadosRoutes = require('./routes/chamados');
 
+//Configuração do middleware JSON:
 app.use(express.json());
+
+//Configuração das rotas:
 app.use('/users', usersRoutes);
 app.use('/chamados', chamadosRoutes);
 
+//Inicialização do servidor:
 app.listen(8080, () => {
     console.log("Servidor iniciado na porta 8080: http://localhost:8080");
 });
